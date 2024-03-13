0cb2694c764b6a2b65d16151e6b35ea49a257f99
Brieuc Dubois <git@bhasher.com>
2024-03-13 19:05:31 +0100

    First draw of session page

---
+++ frontend/src/lib/components/sessions/chatbox.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	let message = '';
+
+	async function sendMessage() {
+		if (message.length == 0) return;
+	}
+</script>
+
+<div id="mainbox">
+	<div id="chatbox"></div>
+	<input
+		type="text"
+		id="chatinput"
+		placeholder="Send a message..."
+		on:keypress={async (e) => {
+			if (e.key === 'Enter') {
+				await sendMessage();
+			}
+		}}
+	/>
+</div>
+
+<style lang="less">
+	#mainbox {
+		display: flex;
+		flex-direction: column;
+		height: 90%;
+		margin-top: 50px;
+		width: clamp(300px, 50%, 800px);
+		border: 2px solid black;
+		border-radius: 5px;
+	}
+
+	#chatbox {
+		flex-grow: 1;
+	}
+
+	#chatinput {
+		height: 50px;
+	}
+</style>
---
+++ frontend/src/routes/session/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Header from '$lib/components/header.svelte';
+	import Chatbox from '$lib/components/sessions/chatbox.svelte';
+	import { requireLogin } from '$lib/utils/login';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		requireLogin();
+	});
+</script>
+
+<div class="container">
+	<Header />
+	<div class="vareas">
+		<Chatbox />
+	</div>
+</div>
+
+<style lang="less">
+	html,
+	body {
+		height: 100%;
+		margin: 0;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.vareas {
+		display: flex;
+		flex-direction: row;
+		flex-grow: 1;
+		justify-content: space-evenly;
+	}
+</style>
