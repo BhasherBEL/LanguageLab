b37b4acddd4dfc03bc9e3272bb56ce7787f9c72c
Brieuc Dubois <git@bhasher.com>
2024-04-13 13:33:59 +0200

    Fix first-login

--- frontend/src/routes/exp/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import User from '$lib/types/user.js';
-
-	export let data;
-	const user = User.parseFromServer(data);
-</script>
-
-<div>Hello world {user?.nickname}</div>
--- frontend/src/routes/first-login/+page.svelte
+++ frontend/src/routes/first-login/+page.svelte
@@ -1,29 +1,21 @@
 <script lang="ts">
-	import { getBaseURL, requireLogin } from '$lib/utils/login';
-	import { onMount } from 'svelte';
-	import { _ } from '$lib/services/i18n';
-	import Header from '$lib/components/header.svelte';
+	import { getBaseURL } from '$lib/utils/login';
+	import { locale, t } from '$lib/services/i18n';
 	import config from '$lib/config';
-	import { _activeLocale } from '$lib/services/i18n';
 	import { createUserMetadataAPI } from '$lib/api/users';
-	import JWTSession from '$lib/stores/JWTSession';
 	import { toastAlert } from '$lib/utils/toasts';
-	import { Language } from 'svelte-hero-icons';
+	import { user } from '$lib/types/user';
 
-	let uiLanguage: string = $_activeLocale;
+	let uiLanguage: string = $locale;
 	let homeLanguage: string;
 	let targetLanguage: string;
 	let birthdate: string;
 	$: isSent = false;
 
-	onMount(() => {
-		if (!requireLogin()) return;
-	});
-
 	async function send(event: Event) {
 		event.preventDefault();
 
-		const user_id = JWTSession.user()?.id;
+		const user_id = $user?.id;
 
 		if (!user_id) {
 			toastAlert('Failed to get current user ID');
@@ -50,15 +42,13 @@
 	}
 </script>
 
-<Header />
-
 <div class=" w-3/4 max-w-3xl flex flex-col justify-center m-auto mt-8">
 	<form class="my-4" on:submit={send}>
 		<div class="text-center">
-			{$_('firstLogin.welcome')}
+			{$t('firstLogin.welcome')}
 		</div>
 		<div class="mt-4">
-			<label for="homeLanguage">{$_('firstLogin.homeLanguage')}</label>
+			<label for="homeLanguage">{$t('firstLogin.homeLanguage')}</label>
 			<select
 				class="input mt-2 w-full bg-transparent"
 				id="homeLanguage"
@@ -72,7 +62,7 @@
 			</select>
 		</div>
 		<div class="mt-4">
-			<label for="targetLanguage">{$_('firstLogin.targetLanguage')}</label>
+			<label for="targetLanguage">{$t('firstLogin.targetLanguage')}</label>
 			<select
 				class="input mt-2 w-full bg-transparent"
 				id="targetLanguage"
@@ -86,7 +76,7 @@
 			</select>
 		</div>
 		<div class="mt-4">
-			<label for="birthdate">{$_('firstLogin.birthdate')}</label>
+			<label for="birthdate">{$t('firstLogin.birthdate')}</label>
 			<input
 				class="input mt-2 w-full"
 				type="date"
@@ -96,18 +86,24 @@
 				bind:value={birthdate}
 			/>
 		</div>
-		<div class="text-center font-bold mt-8">
-			{$_('firstLogin.informedConsent')}
+		<div class="text-center mt-8">
+			{@html $t('firstLogin.informedConsent')}
 		</div>
 		<div class="my-4 flex justify-center">
-			<input class="input mt-2 mr-4" type="checkbox" id="consent" name="consent" required />
-			<label for="consent">{$_('firstLogin.consent')}</label>
+			<input
+				class="input mt-2 mr-4 checkbox checkbox-lg"
+				type="checkbox"
+				id="consent"
+				name="consent"
+				required
+			/>
+			<label for="consent">{$t('firstLogin.consent')}</label>
 		</div>
 		<input
 			type="submit"
 			class="button block m-auto"
 			disabled={isSent}
-			value={$_(isSent ? 'button.thankyou' : 'button.submit')}
+			value={$t(isSent ? 'button.thankyou' : 'button.submit')}
 		/>
 	</form>
 </div>
