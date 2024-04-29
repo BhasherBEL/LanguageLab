9e7b97a4cd90e01a62ec565ea59546a7cdac944f
Brieuc Dubois <git@bhasher.com>
2024-04-29 09:34:14 +0200

    Backend proxy in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		exclude: ['emoji-picker-element']
+	},
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8000'
+		}
 	}
 });
