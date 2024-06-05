71846cfd7914f9d2cf42e6a5926774f3847eec58
Brieuc Dubois <git@bhasher.com>
2024-06-05 12:21:01 +0200

    Implement #46

--- frontend/src/lib/components/sessions/message.svelte
+++ frontend/src/lib/components/sessions/message.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type Message from '$lib/types/message';
 	import { displayTime } from '$lib/utils/date';
-	import { AcademicCap, Check, Icon, Sparkles, User } from 'svelte-hero-icons';
+	import { Check, Icon } from 'svelte-hero-icons';
 	import { user } from '$lib/types/user';
+	import Gravatar from 'svelte-gravatar';
 
 	export let message: Message;
 
@@ -19,14 +20,13 @@
 </script>
 
 <div class="chat" class:chat-start={!isSender} class:chat-end={isSender}>
-	<div class="rounded-full p-3 mx-2 bg-gray-200 chat-image" title={message.user.nickname}>
-		{#if message.user.type == 0}
-			<Icon src={Sparkles} class="w-6 m-auto" />
-		{:else if message.user.type == 1}
-			<Icon src={AcademicCap} class="w-6 m-auto" />
-		{:else}
-			<Icon src={User} class="w-6 m-auto" />
-		{/if}
+	<div class="rounded-full mx-2 chat-image size-12" title={message.user.nickname}>
+		<Gravatar
+			email={message.user.email}
+			size={64}
+			title={message.user.nickname}
+			class="rounded-full"
+		/>
 	</div>
 	<div class="chat-bubble whitespace-pre-wrap" class:chat-bubble-primary={!isSender}>
 		{message.content}
