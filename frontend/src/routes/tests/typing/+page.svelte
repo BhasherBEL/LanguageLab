f2020d562d3b743bb1b3741297a47b5cc3990695
Brieuc Dubois <git@bhasher.com>
2024-07-28 13:26:58 +0200

    Implement #78 and #81

---
+++ frontend/src/routes/tests/typing/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Typingtest from '$lib/components/tests/typingtest.svelte';
+	import { t } from '$lib/services/i18n';
+
+	let finished = false;
+</script>
+
+{#if finished}
+	<p>{$t('surveys.complete')}</p>
+{:else}
+	<Typingtest onFinish={() => (finished = true)} />
+{/if}
