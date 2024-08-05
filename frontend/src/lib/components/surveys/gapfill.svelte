5f05b3d0cbf9672ffa8f7543e5aa455fd1097d89
Brieuc Dubois <git@bhasher.com>
2024-08-05 11:01:43 -0400

    Merge

---
+++ frontend/src/lib/components/surveys/gapfill.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let length;
+	export let onInput: (value: string) => void;
+	let content: string = '';
+</script>
+
+<span class="relative text-blue-500 font-mono tracking-widest px-1"
+	><!--
+	--><input
+		class="absolute bg-transparent text-transparent w-full caret-blue-500 focus:outline-none focus:ring-0"
+		bind:value={content}
+		on:input={(event) => onInput(event.target.value)}
+		maxlength={length}
+	/><!-- 
+	-->{#each Array.from({ length }) as _, i}
+		<label>{content[i] || '_'}</label>
+	{/each}
+</span>
