e027252ce7711d3b1d0d1cd388fd0e0a8de7253d
Brieuc Dubois <git@bhasher.com>
2025-02-12 11:07:50 +0100

    Dev server

--- scripts/update.dev.sh
+++ scripts/update.dev.sh
@@ -1,13 +1,13 @@
-echo "STARTED UPDATE AT $(date)" > /tmp/docker_update
+echo "STARTED DEV UPDATE AT $(date)" > /tmp/docker_update
 
 cd /mnt/data/languagelab/repo/
-git checkout main
+git checkout dev
 git pull
 
 cd /mnt/data/languagelab/repo/
 source app/.env/bin/activate
 cd backend
-alembic -c alembic.prod.ini upgrade head
+alembic -c alembic.dev.ini upgrade head
 
 cd /mnt/data/languagelab/repo/
 docker compose up -d
@@ -15,4 +15,6 @@ docker compose up -d
 cd /mnt/data/languagelab/repo/scripts/surveys
 python3 survey_maker.py < .creds
 
-echo "END UPDATE AT $(date)" >> /tmp/docker_update
+git checkout main
+
+echo "END DEV UPDATE AT $(date)" >> /tmp/docker_update
