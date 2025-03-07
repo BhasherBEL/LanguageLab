68d794da19abff0c92cf172633abcd82798f5049
Brieuc Dubois <git@bhasher.com>
2025-03-07 13:38:54 +0100

    Fix update scripts & dev env

--- scripts/update.dev.sh
+++ scripts/update.dev.sh
@@ -1,20 +1,20 @@
-echo "STARTED DEV UPDATE AT $(date)" > /tmp/docker_update
+echo "STARTED DEV UPDATE AT $(date)" > /tmp/docker_update_dev
 
 cd /mnt/data/languagelab/repo/
-git checkout dev
-git pull
+git checkout dev >> /tmp/docker_update_dev
+git pull >> /tmp/docker_update_dev
+
+
+cd /mnt/data/languagelab/repo/backend 
+source app/.env/bin/activate >> /tmp/docker_update_dev
+alembic -c alembic.dev.ini upgrade head >> /tmp/docker_update_dev
 
 cd /mnt/data/languagelab/repo/
-source app/.env/bin/activate
-cd backend
-alembic -c alembic.dev.ini upgrade head
-
-cd /mnt/data/languagelab/repo/
-docker compose up -d
+docker compose up -d >> /tmp/docker_update_dev
 
 cd /mnt/data/languagelab/repo/scripts/surveys
-python3 survey_maker.py < .creds
+python3 survey_maker.py < .creds >> /tmp/docker_update_dev
 
-git checkout main
+git checkout main >> /tmp/docker_update_dev
 
-echo "END DEV UPDATE AT $(date)" >> /tmp/docker_update
+echo "END DEV UPDATE AT $(date)" >> /tmp/docker_update_dev
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,18 +1,17 @@
 echo "STARTED UPDATE AT $(date)" > /tmp/docker_update
 
 cd /mnt/data/languagelab/repo/
-git checkout main
-git pull
+git checkout main >> /tmp/docker_update
+git pull >> /tmp/docker_update
+
+cd /mnt/data/languagelab/repo/backend
+source app/.env/bin/activate >> /tmp/docker_update
+alembic -c alembic.prod.ini upgrade head >> /tmp/docker_update
 
 cd /mnt/data/languagelab/repo/
-source app/.env/bin/activate
-cd backend
-alembic -c alembic.prod.ini upgrade head
-
-cd /mnt/data/languagelab/repo/
-docker compose up -d
+docker compose up -d >> /tmp/docker_update
 
 cd /mnt/data/languagelab/repo/scripts/surveys
-python3 survey_maker.py < .creds
+python3 survey_maker.py < .creds >> /tmp/docker_update
 
 echo "END UPDATE AT $(date)" >> /tmp/docker_update
