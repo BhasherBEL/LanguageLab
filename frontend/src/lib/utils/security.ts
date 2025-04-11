b94316e31c53fda37b5fe52aaa1d11ac9b9fb58a
Brieuc Dubois <git@bhasher.com>
2025-04-11 09:56:55 +0200

    Fix #151

--- frontend/src/lib/utils/security.ts
+++ frontend/src/lib/utils/security.ts
@@ -28,12 +28,7 @@ export function validateEmail(email: unknown): email is string {
 }
 
 export function validateUsername(username: unknown): username is string {
-	return (
-		typeof username === 'string' &&
-		username.length >= 3 &&
-		username.length <= 31 &&
-		/^[a-z0-9_-]+$/.test(username)
-	);
+	return typeof username === 'string' && username.length >= 3 && username.length <= 31;
 }
 
 export function validatePassword(password: unknown): password is string {
